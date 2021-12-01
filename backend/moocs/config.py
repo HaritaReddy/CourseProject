735e600f8eaddb0f7a76401d3897b40d1d88e923
Harita Reddy <harita_1997@yahoo.com>
2021-12-01 16:57:31 -0600

    Adding mooc config to backend

--- backend/moocs/config.py
+++ backend/moocs/config.py
@@ -1,3 +1,6 @@
 domain_to_university = {
-    'illinois': ('UIUC', 'University of Illinois at Urbana-Champaign')
+    'illinois': ('UIUC', 'University of Illinois at Urbana-Champaign'),
+    'boulder': ('UCB', 'University of Colorado, Boulder'),
+    'uci': ('UCI', 'University of California, Irvine'),
+    'gatech': ('GATECH', 'Georgia Institute of Technology')
 }
